749e5c12a937923f60a8960f638dbb12870a3c36
kithendor <the_kitsos@yahoo.gr>
2019-01-31 09:37:18 +0200

    Create wav.cs

---
+++ wav.cs
@@ -0,0 +1,5 @@
+System.Media.SoundPlayer player =
+            new System.Media.SoundPlayer();
+            player.SoundLocation = @"C:\Users\Thodoris Kitsos\Desktop\s4.wav";
+            player.Load();
+            player.Play();
